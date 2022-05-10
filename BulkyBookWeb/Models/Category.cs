d3cc1f504fb44b44d8d167b299ef379c5389ba5f
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-10 16:53:12 -0700

    Define Default Connection to DB on appsettings.json

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,6 +1,20 @@
-﻿namespace BulkyBookWeb.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
 {
     public class Category
     {
+        //Data Annotation to inform Id column is the primary key
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string DisplayOrder{ get; set; }
+
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+
+
     }
 }
