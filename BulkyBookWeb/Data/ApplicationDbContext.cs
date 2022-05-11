f7e99aea6aaf76b7845fddc4c6b6aac52404e796
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-10 22:27:26 -0700

    Add builder.Services.AddDbContext to Program.cs
    DB Connection

--- BulkyBookWeb/Data/ApplicationDbContext.cs
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
 
-namespace BulkyBookWeb.Data
+namespace BulkyBookWeb.Data;
+
+public class ApplicationDbContext : DbContext
 {
-    public class ApplicationDbContext : DbContext
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
     }
+
+    public DbSet<Category> Categories { get; set; }
 }
+
