eead224e8e973953f1cee2a1e41c7f3c946de469
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-12 15:51:38 -0700

    Add CategoryController to Project and Auto-generate respective View

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers {
+    public class CategoryController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
