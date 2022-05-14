1beef3fb2e2d01fea187dfaf588d130d636b4e58
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-13 17:24:08 -0700

    Add 'Create Category' section

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -3,18 +3,26 @@ using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers;
-    public class CategoryController : Controller {
+public class CategoryController : Controller 
+{
 
     private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext db)
-        {
-            _db = db;
-        }
-
-        public IActionResult Index() {
-            IEnumerable<Category> objCategoryList = _db.Categories;
-            return View(objCategoryList);
-        }
+    public CategoryController(ApplicationDbContext db)
+    {
+        _db = db;
     }
+
+    public IActionResult Index() 
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        return View(objCategoryList);
+    }
+        
+    //GET
+    public IActionResult Create()
+    {        
+        return View();
+    }
+}
 
