5ca30e60f455ec055b3eaae75ddbfd9c3a61bfc6
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-13 17:45:34 -0700

    Add new POST Action to Category Controller with ValidateAntiForgeryToken

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -24,5 +24,15 @@ public class CategoryController : Controller
     {        
         return View();
     }
+
+    //POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        _db.Categories.Add(obj);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
 }
 
