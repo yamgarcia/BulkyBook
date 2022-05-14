b5bbec10959d65eac2334a31f60e91b0493549db
Marcos Garcia <46078084+yamgarcia@users.noreply.github.com>
2022-05-14 07:36:56 -0700

    Add server-side validation to create category action

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -30,9 +30,14 @@ public class CategoryController : Controller
     [ValidateAntiForgeryToken]
     public IActionResult Create(Category obj)
     {
-        _db.Categories.Add(obj);
-        _db.SaveChanges();
-        return RedirectToAction("Index");
+        //Can I modify "The value '' is invalid." before it's sent to view?
+        if (ModelState.IsValid)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        return View(obj);
     }
 }
 
